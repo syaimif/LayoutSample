164d2149fd551f7c73a32da2fa2dce1920fbcf90
Muhammad Miftakhul Syaikhuddin <syaimif@di.blankon.in>
2016-11-22 08:20:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/syaimif/layoutsample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.syaimif.layoutsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Hotel Hilton");
+        getSupportActionBar().setSubtitle("Isla Nublar");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_bookmark){
+            return true;
+        }
+
+        if (id == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
